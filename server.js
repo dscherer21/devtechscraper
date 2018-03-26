ffd848069b26ac1577c1799cd199522e7958e2f4
David Scherer-ODell <dscherer21@gmail.com>
2018-03-26 10:11:45 -0500

    redid folder structure

--- server.js
+++ server.js
@@ -1,113 +1,77 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var morganLog = require('morgan');
-var mongoose = require('mongoose');
-var request = require('request');
-var cheerio = require('cheerio');
-var Port = process.env.PORT || 8080;
+/* Note Taker (18.2.6)
+ * backend
+ * ==================== */
+
+// Dependencies
+var express = require("express");
+var mongojs = require("mongojs");
+var bodyParser = require("body-parser");
+var logger = require("morgan");
+
 var app = express();
 
-//middleware
-app.use(morganLog('dev'));
-app.use(bodyParser.urlencoded({
-  extended: false
-}));
-app.use(express.static('public'));
+// Set the app up with morgan, body-parser, and a static folder
+app.use(logger("dev"));
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
+app.use(express.static("public"));
 
-//set up mongoose Database
-mongoose.connect('mongodb://localhost/hackernews');
-var db = mongoose.connection;
+// Database configuration
+var databaseUrl = "week18day2";
+var collections = ["notes"];
 
-db.on('error', function(err){
-  console.log('Your error is: ', err);
+// Hook mongojs config to db variable
+var db = mongojs(databaseUrl, collections);
+
+// Log any mongojs errors to console
+db.on("error", function(error) {
+  console.log("Database Error:", error);
 });
 
-db.once('open', function(){
-  console.log('Connected to database');
-});
+// Routes
+// ======
 
-//imports
-var myNote = require('./models/myNote.js');
-var myScrape = require('./models/myScrape.js');
-
-//set up get/post requests here
-app.get('/', function(req, res){
+// Simple index route
+app.get("/", function(req, res) {
   res.send(index.html);
 });
 
-//scrape from Hacker news
-app.get('/news', function(req, res){
-  request('https://news.ycombinator.com/', function(err, res, html){
-    if(err){
-      res.send(err)
-    }
-    var $ = cheerio.load(html);
-    var results = {};
-    $('td.title').each(function(index, element){
-      results.title = $(element).text();
-      results.link = $(element).find('a.storylink').first().attr('href');
-      var entry = new myScrape(results);
-      entry.save(function(err, story){
-        if(err){
-          console.log(err);
-        }
-        else{
-          console.log(story);
-        }
-      });
-    });
-  });
-  res.send("scrape successful");
-});
+// TODO: You will make six more routes. Each will use mongojs methods
+// to interact with your mongoDB database, as instructed below.
+// -/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/
 
-//show scraped articles
-app.get('/stories', function(req, res){
-  myScrape.find({}, function(err, story){
-    if(err){
-      console.log(err);
-    }
-    else{
-      res.json(story);
-    }
-  });
-});
+// 1. Save a note to the database's collection
+// POST: /submit
+// ===========================================
+db.notes.insert({'name':'panda', 'numLegs': 4, 'class': 'mammal', 'weight': 254, 'whatIWouldReallyCallIt':'Ninja Bear'})
+// 2. Retrieve all notes from the database's collection
+// GET: /all
+// ====================================================
 
-//grab stories by id
-app.get('/stories/:id', function(req, res){
-  myScrape.findOne({'_id': req.params.id})
-  .populate('notes')
-  .exec(function(err, story){
-    if(err){
-      console.log(err)
-    }
-    else{
-      res.json(story);
-    }
-  });
-});
+// 3. Retrieve one note in the database's collection by it's ObjectId
+// TIP: when searching by an id, the id needs to be passed in
+// as (mongojs.ObjectId(IDYOUWANTTOFIND))
+// GET: /find/:id
+// ==================================================================
 
-//post request for the notes
-app.post('/stories/:id', function(req, res){
-  var newNote = new myNote(req.body);
-  newNote.save(function(err, story){
-    if(err){
-      console.log(err);
-    }
-    else{
-      myScrape.findOneAndUpdate({'_id': req.params.id}, {'note': story._id})
-      .exec(function(err, story){
-        if(err){
-          console.log(err);
-        }
-        else{
-          res.send(story);
-        }
-      });
-    }
-  });
-});
+// 4. Update one note in the database's collection by it's ObjectId
+// (remember, mongojs.ObjectId(IDYOUWANTTOFIND)
+// POST: /update/:id
+// ================================================================
 
-//fire up the server
-app.listen(Port, function(){
-  console.log('Server running on Port: ' + Port);
+// 5. Delete one note from the database's collection by it's ObjectId
+// (remember, mongojs.ObjectId(IDYOUWANTTOFIND)
+// GET: /delete/:id
+// ==================================================================
+
+// 6. Clear the entire note collection
+// GET: /clearall
+// ===================================
+
+// Listen on port 3000
+app.listen(3000, function() {
+  console.log("App running on port 3000!");
 });
